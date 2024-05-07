fb345531ddf3adabd89f790c40889d71f8de4420
cjs8487 <cjschaefer1999@gmail.com>
2024-05-06 19:55:16 -0500

    racetime configuration

--- web/src/app/(main)/games/[slug]/page.tsx
+++ web/src/app/(main)/games/[slug]/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 import { useApi } from '@/lib/Hooks';
 import { Game } from '@/types/Game';
-import Info from '@mui/icons-material/Info';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { Box, Button, Container, Link, Tab, Typography } from '@mui/material';
-import { Form, Formik } from 'formik';
+import { Box, Container, Link, Tab, Typography } from '@mui/material';
 import Image from 'next/image';
 import NextLink from 'next/link';
 import { useLayoutEffect, useState } from 'react';
-import { mutate } from 'swr';
-import HoverIcon from '../../../../components/HoverIcon';
+import GameSettings from '../../../../components/game/GameSettings';
 import PermissionsManagement from '../../../../components/game/PermissionsManagement';
 import GoalManagement from '../../../../components/game/goals/GoalManagement';
-import FormikSwitch from '../../../../components/input/FormikSwitch';
-import FormikTextField from '../../../../components/input/FormikTextField';
-import { alertError } from '../../../../lib/Utils';
 import { GoalManagerContextProvider } from '../../../../context/GoalManagerContext';
+import { alertError } from '../../../../lib/Utils';
 
 export default function GamePage({
     params: { slug },
@@ -133,95 +128,7 @@ export default function GamePage({
                     <PermissionsManagement slug={slug} gameData={gameData} />
                 </TabPanel>
                 <TabPanel value="Settings">
-                    <div>
-                        <Typography variant="h5" align="center">
-                            Game Settings
-                        </Typography>
-                        <Formik
-                            initialValues={{
-                                name: gameData.name,
-                                coverImage: gameData.coverImage,
-                                enableSRLv5: gameData.enableSRLv5,
-                            }}
-                            onSubmit={async ({
-                                name,
-                                coverImage,
-                                enableSRLv5,
-                            }) => {
-                                const res = await fetch(`/api/games/${slug}`, {
-                                    method: 'POST',
-                                    body: JSON.stringify({
-                                        name,
-                                        coverImage,
-                                        enableSRLv5,
-                                    }),
-                                });
-                                if (!res.ok) {
-                                    const error = await res.text();
-                                    alertError(
-                                        `Failed to update game - ${error}`,
-                                    );
-                                    return;
-                                }
-                                mutate(`/api/games/${slug}`);
-                            }}
-                        >
-                            <Form>
-                                <Box
-                                    display="flex"
-                                    flexDirection="column"
-                                    justifyItems="center"
-                                    rowGap={2}
-                                    pt={2}
-                                >
-                                    <FormikTextField
-                                        id="game-name"
-                                        name="name"
-                                        label="Name"
-                                    />
-                                    <FormikTextField
-                                        id="game-cover-image"
-                                        name="coverImage"
-                                        label="Cover Image"
-                                    />
-                                    <Box display="flex" alignItems="center">
-                                        <FormikSwitch
-                                            id="game-srlv5-generation-switch"
-                                            label="Enable SRLv5 Board Generation"
-                                            name="enableSRLv5"
-                                        />
-                                        <HoverIcon icon={<Info />}>
-                                            <Typography variant="caption">
-                                                SRLv5 generation requires goals
-                                                to have a difficulty value
-                                                assigned to them in order to be
-                                                used in generation. The
-                                                generator uses the difficulty
-                                                value to balance each row,
-                                                column, and diagonal, by having
-                                                the difficulty of goals in each
-                                                sum to the same value. It also
-                                                tries to minimize synergy
-                                                between goals in the same line
-                                                by minimizing the category
-                                                overlap.
-                                            </Typography>
-                                        </HoverIcon>
-                                    </Box>
-                                    <Box pt={1} display="flex">
-                                        <Box flexGrow={1} />
-                                        <Button
-                                            type="submit"
-                                            variant="contained"
-                                            color="success"
-                                        >
-                                            Save Changes
-                                        </Button>
-                                    </Box>
-                                </Box>
-                            </Form>
-                        </Formik>
-                    </div>
+                    <GameSettings gameData={gameData} />
                 </TabPanel>
             </TabContext>
         </Container>
