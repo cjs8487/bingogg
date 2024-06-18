bd3941637fb349c0ced67eabb996aa8abbbe72c9
cjs8487 <cjschaefer1999@gmail.com>
2024-06-18 18:32:43 -0500

    footer

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body style={{}}>
                 <AppRouterCacheProvider options={{ enableCssLayer: true }}>
                     <ThemeProvider theme={theme}>
                         <CssBaseline />
