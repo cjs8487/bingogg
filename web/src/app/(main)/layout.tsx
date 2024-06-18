eb4ed76001f4375cc39ec809ab1bc7129645377e
cjs8487 <cjschaefer1999@gmail.com>
2024-06-18 18:32:43 -0500

    footer

--- web/src/app/(main)/layout.tsx
+++ web/src/app/(main)/layout.tsx
@@ -7,12 +7,13 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { ReactNode } from 'react';
 import CookieConsent from 'react-cookie-consent';
 import { ToastContainer } from 'react-toastify';
 import Header from '../../components/header/Header';
 import 'react-toastify/dist/ReactToastify.css';
+import { Box, Container, Link, Paper, Typography } from '@mui/material';
 
 const icons: { icon: IconDefinition; url: string }[] = [
     { icon: faGithub, url: 'https://github.com/cjs8487/bingogg' },
@@ -23,7 +24,13 @@ const icons: { icon: IconDefinition; url: string }[] = [
 
 export default function CoreLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="h-screen bg-background text-text">
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+            }}
+        >
             <Header />
             <CookieConsent
                 location="bottom"
@@ -37,34 +44,45 @@ export default function CoreLayout({ children }: { children: ReactNode }) {
                 This website uses cookies to provide some parts of it&#39;s
                 functionality.
             </CookieConsent>
-            <div className="h-[calc(100%-60px)] p-4">{children}</div>
-            <div className="bg-background">
-                <div className="flex items-end rounded-t-md bg-primary-light p-2">
-                    <div className="grow">
-                        <div className="mb-2 flex items-center gap-x-1 text-xl">
+            <Box flexGrow={1}>{children}</Box>
+            <Paper component="footer">
+                <Box sx={{ display: 'flex', alignItems: 'end', p: 2 }}>
+                    <Box flexGrow={1}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                columnGap: 1,
+                                mb: 0,
+                            }}
+                        >
                             {icons.map(({ icon, url }) => (
-                                <a key={icon.iconName} href={url}>
+                                <Link key={icon.iconName} href={url}>
                                     <FontAwesomeIcon
                                         icon={icon}
                                         className="fa-fw aspect-square rounded-full px-1 py-1.5 hover:bg-primary-content hover:bg-opacity-40"
                                     />
-                                </a>
+                                </Link>
                             ))}
-                        </div>
-                        <div className="text-xs">
-                            <div></div>© Copyright 2024 - 2024 Bingothon | All
-                            Rights Reserved |{' '}
-                            <Link href="/legal/privacy" className="underline">
+                        </Box>
+                        <Typography variant="caption">
+                            © Copyright 2024 - 2024 Bingothon | All Rights
+                            Reserved |{' '}
+                            <Link
+                                href="/legal/privacy"
+                                component={NextLink}
+                                className="underline"
+                            >
                                 Privacy Policy
                             </Link>
-                        </div>
-                    </div>
-                    <div className="pr-1 text-xs">
+                        </Typography>
+                    </Box>
+                    <Typography variant="caption">
                         bingo.gg v{process.env.version}
-                    </div>
-                </div>
-            </div>
+                    </Typography>
+                </Box>
+            </Paper>
             <ToastContainer />
-        </div>
+        </Box>
     );
 }
