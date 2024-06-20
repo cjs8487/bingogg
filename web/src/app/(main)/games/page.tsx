510921c031d795c1bfb5738b26489fb3a43eb559
cjs8487 <cjschaefer1999@gmail.com>
2024-06-19 22:53:40 -0500

    games page

--- web/src/app/(main)/games/page.tsx
+++ web/src/app/(main)/games/page.tsx
@@ -4,6 +4,16 @@ import { Game } from '@/types/Game';
 import { useApi } from '../../../lib/Hooks';
 import { useContext } from 'react';
 import { UserContext } from '../../../context/UserContext';
+import {
+    Box,
+    Button,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Typography,
+} from '@mui/material';
+import Masonry from '@mui/lab/Masonry';
 
 export default function Games() {
     const { loggedIn } = useContext(UserContext);
@@ -20,46 +30,87 @@ export default function Games() {
 
     return (
         <>
-            <div className="flex items-center border-b pb-4">
-                {games.length} games loaded
-                <div className="grow" />
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    mb: 4,
+                    p: 1,
+                    borderBottom: '1px solid',
+                }}
+                className="flex items-center border-b pb-4"
+            >
+                <Typography>{games.length} games loaded</Typography>
+                <Box flexGrow={1} />
                 {loggedIn && (
                     <div>
-                        <Link
+                        <Button
                             className="rounded-md border bg-green-700 p-2"
                             href="/games/new"
+                            LinkComponent={Link}
                         >
                             Create a new game
-                        </Link>
+                        </Button>
                     </div>
                 )}
-            </div>
-            <div className="flex flex-wrap gap-x-4 gap-y-4 pt-2">
-                {games.map((game) => (
-                    <Link
-                        key={game.slug}
-                        className="flex basis-1/6 flex-col gap-y-1"
-                        href={`/games/${game.slug}`}
-                    >
-                        {game.coverImage && (
-                            <div
-                                className="bg-cover bg-center bg-no-repeat pt-[133%]"
-                                style={{
-                                    backgroundImage: `url(${game.coverImage})`,
-                                }}
-                            />
-                        )}
-                        {!game.coverImage && (
-                            <div className="relative flex border pt-[133%] text-4xl shadow-[inset_0_0_12px_white]">
-                                <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center">
-                                    {game.slug}
-                                </div>
-                            </div>
-                        )}
-                        <div>{game.name}</div>
-                    </Link>
-                ))}
-            </div>
+            </Box>
+            <Box p={0}>
+                <Masonry
+                    columns={{ xs: 1, sm: 2, md: 4, lg: 5, xl: 6 }}
+                    spacing={2}
+                >
+                    {games.map((game) => (
+                        <Card key={game.slug}>
+                            <CardActionArea
+                                href={`/games/${game.slug}`}
+                                LinkComponent={Link}
+                            >
+                                {game.coverImage && (
+                                    <CardMedia
+                                        component="img"
+                                        image={game.coverImage}
+                                    />
+                                )}
+                                {!game.coverImage && (
+                                    <Box
+                                        sx={{
+                                            position: 'relative',
+                                            display: 'flex',
+                                            border: '1px',
+                                            pt: '80%',
+                                            boxShadow: 'inset 0 0 12px',
+                                        }}
+                                    >
+                                        <Typography
+                                            sx={{
+                                                position: 'absolute',
+                                                bottom: 0,
+                                                left: 0,
+                                                right: 0,
+                                                top: 0,
+                                                display: 'flex',
+                                                alignItems: 'center',
+                                                justifyContent: 'center',
+                                            }}
+                                        >
+                                            {game.slug}
+                                        </Typography>
+                                    </Box>
+                                )}
+                                <CardContent>
+                                    <Typography
+                                        variant="h6"
+                                        textAlign="center"
+                                        // pt={2}
+                                    >
+                                        {game.name}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    ))}
+                </Masonry>
+            </Box>
         </>
     );
 }
