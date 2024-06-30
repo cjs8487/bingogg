d32b5783f3c68ae7f47ad6673b526ab1d2ec584e
cjs8487 <cjschaefer1999@gmail.com>
2024-06-30 13:51:59 -0500

    add spacing between room components

--- web/src/app/(main)/rooms/[slug]/page.tsx
+++ web/src/app/(main)/rooms/[slug]/page.tsx
@@ -27,74 +27,72 @@ export default function Room() {
     }
 
     return (
-        <Box flex="column" flexGrow={1}>
+        <Box flex="column" flexGrow={1} p={2}>
             <AutoSizer>
-                {({ width, height }) => {
-                    console.log(height);
-                    return (
+                {({ width, height }) => (
+                    <Box
+                        width={width}
+                        height={height}
+                        display="flex"
+                        flexDirection="column"
+                    >
                         <Box
-                            width={width}
-                            height={height}
                             display="flex"
-                            flexDirection="column"
+                            columnGap={2}
+                            maxHeight="30%"
+                            pb={2}
                         >
-                            <Box
-                                display="flex"
-                                maxHeight="30%"
-                                // className="flex h-[30%] gap-x-4 pb-4"
-                            >
-                                <Box>
-                                    <RoomInfo />
-                                </Box>
-                                <Box>
-                                    <Card className="flex h-fit flex-col gap-y-3 rounded-md border border-border bg-foreground p-3">
-                                        <CardContent>
-                                            <Box
-                                                display="flex"
-                                                alignItems="center"
-                                                flexGrow={1}
-                                            >
-                                                <Typography
-                                                    variant="h6"
-                                                    flexGrow={1}
-                                                    className="float-left text-lg font-semibold"
-                                                >
-                                                    Playing as {nickname}
-                                                </Typography>
-                                                {connectionStatus !==
-                                                    ConnectionStatus.CLOSED && (
-                                                    <Button
-                                                        className="float-right rounded-md border bg-background px-2 py-1 shadow-md shadow-white/20 hover:bg-border"
-                                                        onClick={disconnect}
-                                                    >
-                                                        Disconnect
-                                                    </Button>
-                                                )}
-                                            </Box>
-                                            <Box className="rounded-md border bg-background p-2 shadow-md shadow-white/20">
-                                                <Typography className="pb-1 font-semibold">
-                                                    Choose your color
-                                                </Typography>
-                                                <ColorSelect />
-                                            </Box>
-                                        </CardContent>
-                                    </Card>
-                                </Box>
+                            <Box>
+                                <RoomInfo />
                             </Box>
-                            <Box display="flex" maxHeight="70%" columnGap={8}>
-                                <Box flexGrow={1} maxWidth="50%">
-                                    <Board />
-                                </Box>
-                                <Box>
-                                    <RoomChat />
-                                </Box>
-                                <Box>
-                                    <PlayerList />
-                                </Box>
+                            <Box>
+                                <Card className="flex h-fit flex-col gap-y-3 rounded-md border border-border bg-foreground p-3">
+                                    <CardContent>
+                                        <Box
+                                            display="flex"
+                                            alignItems="center"
+                                            flexGrow={1}
+                                        >
+                                            <Typography
+                                                variant="h6"
+                                                flexGrow={1}
+                                                className="float-left text-lg font-semibold"
+                                            >
+                                                Playing as {nickname}
+                                            </Typography>
+                                            {connectionStatus !==
+                                                ConnectionStatus.CLOSED && (
+                                                <Button
+                                                    className="float-right rounded-md border bg-background px-2 py-1 shadow-md shadow-white/20 hover:bg-border"
+                                                    onClick={disconnect}
+                                                >
+                                                    Disconnect
+                                                </Button>
+                                            )}
+                                        </Box>
+                                        <Box className="rounded-md border bg-background p-2 shadow-md shadow-white/20">
+                                            <Typography className="pb-1 font-semibold">
+                                                Choose your color
+                                            </Typography>
+                                            <ColorSelect />
+                                        </Box>
+                                    </CardContent>
+                                </Card>
                             </Box>
                         </Box>
-                    );
-                }}
+                        <Box display="flex" maxHeight="70%" columnGap={8}>
+                            <Box flexGrow={1} maxWidth="50%">
+                                <Board />
+                            </Box>
+                            <Box>
+                                <RoomChat />
+                            </Box>
+                            <Box>
+                                <PlayerList />
+                            </Box>
+                        </Box>
+                    </Box>
+                )}
             </AutoSizer>
         </Box>
     );
