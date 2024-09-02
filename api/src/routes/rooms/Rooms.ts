d44c3b1c5399da59364cf7a9d463e171f417469c
cjs8487 <cjschaefer1999@gmail.com>
2024-09-01 21:02:31 -0500

    basic new action framework

--- api/src/routes/rooms/Rooms.ts
+++ api/src/routes/rooms/Rooms.ts
@@ -1,6 +1,6 @@
 import { randomInt } from 'crypto';
 import { Router } from 'express';
-import { createRoomToken } from '../../auth/RoomAuth';
+import { createRoomToken, verifyRoomToken } from '../../auth/RoomAuth';
 import Room, { BoardGenerationMode } from '../../core/Room';
 import { allRooms } from '../../core/RoomServer';
 import {
@@ -10,7 +10,7 @@ import {
 } from '../../database/Rooms';
 import { gameForSlug, goalCount } from '../../database/games/Games';
 import { chunk } from '../../util/Array';
-import actions from './Actions';
+import { logInfo, logWarn } from '../../Logger';
 
 const MIN_ROOM_GOALS_REQUIRED = 25;
 const rooms = Router();
@@ -214,6 +214,77 @@ rooms.post('/:slug/authorize', (req, res) => {
     res.status(200).send({ authToken: token });
 });
 
-rooms.use('/actions', actions);
+interface ActionResultBase {
+    code: number;
+}
+
+interface ActionResultError extends ActionResultBase {
+    message: string;
+}
+
+interface ActionResult<T> extends ActionResultBase {
+    value: T;
+}
+
+rooms.post<{ slug: string; action: string }>(
+    '/:slug/actions/:action(*)',
+    (req, res) => {
+        const { slug, action } = req.params;
+
+        if (!req.session.user) {
+            logWarn(`Unauthorized action request ${action}`);
+            res.sendStatus(401);
+            return;
+        }
+
+        const { authToken } = req.body;
+        if (!authToken) {
+            logInfo(`Malformed action body request - missing authToken`);
+            res.status(400).send('Missing required body parameter');
+            return;
+        }
+
+        const room = allRooms.get(slug);
+        if (!room) {
+            logInfo(`Unable to find room to take action on`);
+            res.sendStatus(404);
+            return;
+        }
+        if (!verifyRoomToken(authToken, slug)) {
+            room.logWarn(`Unauthorized action request`);
+            res.sendStatus(403);
+            return;
+        }
+
+        const result = handleAction(room, action, req.session.user);
+
+        res.status(result.code);
+        if ('message' in result) {
+            res.send(result.message);
+        } else {
+            res.json(result.value);
+        }
+    },
+);
+
+const handleAction = (
+    room: Room,
+    action: string,
+    user: string,
+): ActionResult<unknown> | ActionResultError => {
+    switch (action) {
+        case 'racetime/create':
+            return {
+                code: 200,
+                value: { url: '' },
+            };
+        default:
+            room.logInfo('Unknown action request');
+            return {
+                code: 400,
+                message: 'Unknown action',
+            };
+    }
+};
 
 export default rooms;
