ba5cc54c8b6874eec97e3980e780f132eb42e1e7
cjs8487 <cjschaefer1999@gmail.com>
2026-09-14 22:57:25 -0500

    use discrete spectator button for room login

--- web/src/components/room/RoomLogin.tsx
+++ web/src/components/room/RoomLogin.tsx
@@ -3,7 +3,6 @@ import { Box, Button, Typography } from '@mui/material';
 import { Form, Formik } from 'formik';
 import { useContext, useState } from 'react';
 import { RoomContext } from '../../context/RoomContext';
-import FormikSwitch from '../input/FormikSwitch';
 import FormikTextField from '../input/FormikTextField';
 
 export default function RoomLogin() {
@@ -23,11 +22,7 @@ export default function RoomLogin() {
             }}
         >
             <Formik
-                initialValues={{
-                    nickname: '',
-                    password: '',
-                    spectator: false,
-                }}
+                initialValues={{ nickname: '', password: '', spectator: false }}
                 onSubmit={async ({ nickname, password, spectator }) => {
                     const result = await connect(nickname, password, spectator);
                     if (!result.success) {
@@ -35,55 +30,68 @@ export default function RoomLogin() {
                     }
                 }}
             >
-                <Form>
-                    {error && (
-                        <Typography
-                            color="error"
-                            variant="body2"
-                            sx={{
-                                pb: 1,
-                            }}
-                        >
-                            {error}
-                        </Typography>
-                    )}
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            rowGap: 2,
-                        }}
-                    >
-                        <FormikTextField
-                            id="nickname"
-                            name="nickname"
-                            label="Nickname"
-                        />
-                        <FormikTextField
-                            id="password"
-                            name="password"
-                            type="password"
-                            label="Password"
-                        />
-                        <FormikSwitch
-                            name="spectator"
-                            id="spectator"
-                            label="Join as spectator?"
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                        }}
-                    >
+                {({ setFieldValue, submitForm }) => (
+                    <Form>
+                        {error && (
+                            <Typography
+                                color="error"
+                                variant="body2"
+                                sx={{
+                                    pb: 1,
+                                }}
+                            >
+                                {error}
+                            </Typography>
+                        )}
                         <Box
                             sx={{
-                                flexGrow: 1,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                rowGap: 2,
                             }}
-                        />
-                        <Button type="submit">Join Room</Button>
-                    </Box>
-                </Form>
+                        >
+                            <FormikTextField
+                                id="nickname"
+                                name="nickname"
+                                label="Nickname"
+                            />
+                            <FormikTextField
+                                id="password"
+                                name="password"
+                                type="password"
+                                label="Password"
+                            />
+                        </Box>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'flex-end',
+                                gap: 1,
+                                mt: 2,
+                            }}
+                        >
+                            <Button
+                                type="button"
+                                color="secondary"
+                                onClick={async () => {
+                                    await setFieldValue('spectator', true);
+                                    await submitForm();
+                                }}
+                            >
+                                Join as spectator
+                            </Button>
+                            <Button
+                                type="button"
+                                onClick={async () => {
+                                    await setFieldValue('spectator', false);
+                                    await submitForm();
+                                }}
+                            >
+                                Join Room
+                            </Button>
+                        </Box>
+                    </Form>
+                )}
             </Formik>
         </Box>
     );
