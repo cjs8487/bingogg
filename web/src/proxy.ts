0deb4c2db0dfd7bd80891fe043c90c21ba01a59d
cjs8487 <cjschaefer1999@gmail.com>
2026-09-14 23:44:01 -0500

    proxy fix

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -2,10 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function proxy(request: NextRequest) {
+    console.log(request.url);
     const headers = new Headers(request.headers);
     headers.set('Content-Type', 'application/json');
     return NextResponse.rewrite(
-        `${process.env.NEXT_PUBLIC_API_PATH}/${request.nextUrl.pathname}${
+        `${process.env.NEXT_PUBLIC_API_PATH}${request.nextUrl.pathname}${
             request.nextUrl.search ? `?${request.nextUrl.search}` : ''
         }`,
         { request: { headers } },
